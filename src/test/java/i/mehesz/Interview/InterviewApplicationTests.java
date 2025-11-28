abf73379cc21e1a9d91e583455d5c8c571b636cf
draiiz <isii.mehesz@gmail.com>
2025-11-28 11:24:32 +0100

    Initilized Project

---
+++ src/test/java/i/mehesz/Interview/InterviewApplicationTests.java
@@ -0,0 +1,15 @@
+package i.mehesz.Interview;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
